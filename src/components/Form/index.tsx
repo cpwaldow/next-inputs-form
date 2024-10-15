fe0852f7ff40bf67dd3317fb286c0f2783970b1a
Carlos Eduardo <cpwaldow@gmail.com>
2024-10-15 05:10:16 -0300

    commit inicial

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,48 @@
+import { Container, Title, FormStyle } from './style';
+import { Button, Input } from '@mui/material';
+
+const Form = () => {
+  return (
+    <div style={Container}>
+      <h2 style={Title}>Teste de formulário</h2>
+      <form style={FormStyle}>
+        <Input
+          placeholder='Nome'
+          disableUnderline
+          type='text'
+          sx={{ border: '1px solid black', p: 1 }}
+        />
+        <Input
+          placeholder='Email'
+          disableUnderline
+          type='email'
+          sx={{ border: '1px solid black', p: 1 }}
+          inputMode='email'
+        />
+        <Input
+          placeholder='Telefone'
+          disableUnderline
+          type='tel'
+          sx={{ border: '1px solid black', p: 1 }}
+        />
+        <Input
+          placeholder='CEP'
+          disableUnderline
+          type='text'
+          sx={{ border: '1px solid black', p: 1 }}
+          inputMode='numeric'
+        />
+        <Input
+          placeholder='Cartão de crédito'
+          disableUnderline
+          type='text'
+          sx={{ border: '1px solid black', p: 1 }}
+          inputMode='numeric'
+        />
+        <Button variant='contained'>teste</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/Form/style.ts
@@ -0,0 +1,17 @@
+import { CSSProperties } from 'react';
+
+export const Container = {
+  fontFamily: 'Arial, sans-serif',
+};
+
+export const Title: CSSProperties = {
+  textAlign: 'center',
+};
+
+export const FormStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '70%',
+  margin: 'auto',
+  gap: 20,
+};
